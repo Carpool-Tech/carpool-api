29eeddd453c1524804bedcca3c89f46058511c92
Patrick Nekel <nekelpatrick@gmail.com>
2023-08-02 02:57:07 -0300

    fix: provide default values for all fields in User, Car, Trip, and UserRoute models

--- carpool/models.py
+++ carpool/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
     is_driver = models.BooleanField(default=False)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True, default="")
     birth_date = models.DateField(null=True, blank=True)
     smoking = models.BooleanField(default=False)
     pets = models.BooleanField(default=False)
@@ -42,12 +43,12 @@ class Car(models.Model):
     ]
 
     owner = models.ForeignKey(User, related_name="cars", on_delete=models.CASCADE)
-    model = models.CharField(max_length=100)
-    seats = models.IntegerField()
-    plate = models.CharField(max_length=10)
-    fuel_type = models.CharField(max_length=10, choices=FUEL_TYPES)
-    year = models.IntegerField()
-    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=100, default="Unknown")
+    seats = models.IntegerField(default=4)
+    plate = models.CharField(max_length=10, default="")
+    fuel_type = models.CharField(max_length=10, choices=FUEL_TYPES, default="gasoline")
+    year = models.IntegerField(default=datetime.now().year)
+    brand = models.CharField(max_length=50, default="Unknown")
     last_maintenance = models.DateField(null=True, blank=True)
 
 
@@ -56,21 +57,22 @@ class Trip(models.Model):
         User, related_name="driven_trips", on_delete=models.CASCADE
     )
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    actual_start_location = models.CharField(max_length=200)
-    actual_end_location = models.CharField(max_length=200)
-    actual_start_time = models.DateTimeField()
-    actual_end_time = models.DateTimeField()
-    actual_total_time = models.DurationField()
-    total_carpool_tip = models.FloatField()
+    actual_start_location = models.CharField(max_length=200, default="")
+    actual_end_location = models.CharField(max_length=200, default="")
+    actual_start_time = models.DateTimeField(null=True, blank=True)
+    actual_end_time = models.DateTimeField(null=True, blank=True)
+    actual_total_time = models.DurationField(null=True, blank=True)
+    total_carpool_tip = models.FloatField(default=0)
     is_finished = models.BooleanField(default=False)
 
 
 class UserRoute(models.Model):
     user = models.ForeignKey(User, related_name="user_routes", on_delete=models.CASCADE)
-    start_location = models.CharField(max_length=200)
-    end_location = models.CharField(max_length=200)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_location = models.CharField(max_length=200, default="")
+    end_location = models.CharField(max_length=200, default="")
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
     max_divert_distance = models.FloatField(
-        help_text="Maximum distance the user is willing to divert from the route, in km"
+        help_text="Maximum distance the user is willing to divert from the route, in km",
+        default=0,
     )
